4557347d0f5e0f88157354418664d31cbd314c6d
Aditya <adityachatto14@gmail.com>
2023-02-10 10:32:58 -0500

    Added 3 cards per row, added margin between cards. Used js ternary operator to display employee image

--- src/Employees.js
+++ src/Employees.js
@@ -92,28 +92,27 @@ const Employees = () => {
       }])
     return (
         <main className="container">
-          <div className="row">
+          <div className="row justify-content-center mt-3 mb-3">
             <div className="col-8">
+              <div className="card-collection">
               {
                 employees.map((employee) => (
-                  <div id="{employee.id}" className="card">
-                    <img src={femaleProfile} alt="Unable to find image" className="card-img-top"/>
+                  // style - cursor..when hovering above this element it changes the cursor display to a hand click me icon
+                  <div id="{employee.id}" className="card m-2" style={{cursor: "pointer"}}>
+
+                    {(employee.gender === 'male')?<img src={maleProfile} alt="Unable to find image" className="card-img-top"/> :
+                                                  <img src={femaleProfile} alt="Unable to find image" className="card-img-top"/>}
 
                     <div className="card-body">
                       <h5 className="card-title">Full Name: {employee.fullName}</h5>
                       <p className="card-text"><b>Designation:</b> {employee.designation}</p>
-                    </div>
-
-                    
+                    </div>            
                   </div>
-
-
               ))
            }
-
+              </div>
             </div>
           </div>
-
         </main>
     )
 }

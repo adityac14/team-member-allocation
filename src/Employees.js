f638d7f1141dd3000f236a1d714c1db00b522ee7
adityac14 <adityachatto14@gmail.com>
2023-02-10 13:41:42 -0500

    Added drop-down list with default value set to TeamB. Managed state of drop-down list selection

--- src/Employees.js
+++ src/Employees.js
@@ -3,6 +3,9 @@ import femaleProfile from './images/femaleProfile.jpg'
 import maleProfile from './images/maleProfile.jpg'
 
 const Employees = () => {
+
+  const [selectedTeam, setTeam] = useState("TeamB")
+
   // employees = employee data. the state of the employee data is maintained in the employees variable
   // setEmployees is a function reference and is used where ever we need to change the state of the employees array
   // useState hook returns a pair of values the current state and a function that updates it
@@ -90,8 +93,28 @@ const Employees = () => {
         gender: "male",
         teamName: "TeamD"
       }])
+
+      function handleTeamSelectionChange(event){
+        console.log(event.target.value)
+        setTeam(event.target.value);
+        
+      }
     return (
         <main className="container">
+
+          <div className="row justify-content-center mt-3 mb-3">
+
+            <div className="col-6">
+              <select className="form-select form-select-lg" value={selectedTeam} onChange={handleTeamSelectionChange}>
+                <option value="TeamA">TeamA</option>
+                <option value="TeamB">TeamB</option>
+                <option value="TeamC">TeamC</option>
+                <option value="TeamD">TeamD</option>
+
+              </select>
+            </div>
+
+          </div>
           <div className="row justify-content-center mt-3 mb-3">
             <div className="col-8">
               <div className="card-collection">

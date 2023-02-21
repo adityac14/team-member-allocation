64e8bc473f7097aff2e8500de2d3748386c22c06
Aditya <adityachatto14@gmail.com>
2023-02-20 22:47:14 -0500

    Moved UseState Logic from Employee Component to App(Parent) Component. Used prop destructuring.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './Header';
-import Footer from './Footer';
-import Employees from './Employees';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Employees from "./Employees";
+import { useState } from "react";
 
 function App() {
+  const [selectedTeam, setTeam] = useState("TeamB");
+
+  // employees = employee data. the state of the employee data is maintained in the employees variable
+  // setEmployees is a function reference and is used where ever we need to change the state of the employees array
+  // useState hook returns a pair of values the current state and a function that updates it
+  const [employees, setEmployees] = useState([
+    {
+      id: 1,
+      fullName: "Bob Jones",
+      designation: "JavaScript Developer",
+      gender: "male",
+      teamName: "TeamA",
+    },
+    {
+      id: 2,
+      fullName: "Jill Bailey",
+      designation: "Node Developer",
+      gender: "female",
+      teamName: "TeamA",
+    },
+    {
+      id: 3,
+      fullName: "Gail Shepherd",
+      designation: "Java Developer",
+      gender: "female",
+      teamName: "TeamA",
+    },
+    {
+      id: 4,
+      fullName: "Sam Reynolds",
+      designation: "React Developer",
+      gender: "male",
+      teamName: "TeamB",
+    },
+    {
+      id: 5,
+      fullName: "David Henry",
+      designation: "DotNet Developer",
+      gender: "male",
+      teamName: "TeamB",
+    },
+    {
+      id: 6,
+      fullName: "Sarah Blake",
+      designation: "SQL Server DBA",
+      gender: "female",
+      teamName: "TeamB",
+    },
+    {
+      id: 7,
+      fullName: "James Bennet",
+      designation: "Angular Developer",
+      gender: "male",
+      teamName: "TeamC",
+    },
+    {
+      id: 8,
+      fullName: "Jessica Faye",
+      designation: "API Developer",
+      gender: "female",
+      teamName: "TeamC",
+    },
+    {
+      id: 9,
+      fullName: "Lita Stone",
+      designation: "C++ Developer",
+      gender: "female",
+      teamName: "TeamC",
+    },
+    {
+      id: 10,
+      fullName: "Daniel Young",
+      designation: "Python Developer",
+      gender: "male",
+      teamName: "TeamD",
+    },
+    {
+      id: 11,
+      fullName: "Adrian Jacobs",
+      designation: "Vue Developer",
+      gender: "male",
+      teamName: "TeamD",
+    },
+    {
+      id: 12,
+      fullName: "Devin Monroe",
+      designation: "Graphic Designer",
+      gender: "male",
+      teamName: "TeamD",
+    },
+  ]);
+
+  function handleTeamSelectionChange(event) {
+    console.log(event.target.value);
+    setTeam(event.target.value);
+  }
+
+  function handleEmployeeCardClick(event) {
+    const transformedEmployees = employees.map((employee) =>
+      employee.id === parseInt(event.currentTarget.id)
+        ? employee.teamName === selectedTeam
+          ? { ...employee, teamName: "" }
+          : { ...employee, teamName: selectedTeam }
+        : employee
+    );
+
+    setEmployees(transformedEmployees);
+  }
+
   return (
     <div>
-      <Header/>
-      <Employees/>
-      <Footer/>
+      <Header />
+      <Employees
+        employees={employees}
+        selectedTeam={selectedTeam}
+        handleEmployeeCardClick={handleEmployeeCardClick}
+        handleTeamSelectionChange={handleTeamSelectionChange}
+      />
+      <Footer />
     </div>
   );
 }
--- src/Employees.js
+++ src/Employees.js
@@ -1,113 +1,11 @@
-import { useState } from "react";
 import femaleProfile from './images/femaleProfile.jpg'
 import maleProfile from './images/maleProfile.jpg'
 
-const Employees = () => {
-
-  const [selectedTeam, setTeam] = useState("TeamB")
-
-  // employees = employee data. the state of the employee data is maintained in the employees variable
-  // setEmployees is a function reference and is used where ever we need to change the state of the employees array
-  // useState hook returns a pair of values the current state and a function that updates it
-    const [employees, setEmployees] = useState([{
-        id: 1,
-        fullName: "Bob Jones",
-        designation: "JavaScript Developer",
-        gender: "male",
-        teamName: "TeamA"
-      },
-      {
-        id: 2,
-        fullName: "Jill Bailey",
-        designation: "Node Developer",
-        gender: "female",
-        teamName: "TeamA"
-      },
-      {
-        id: 3,
-        fullName: "Gail Shepherd",
-        designation: "Java Developer",
-        gender: "female",
-        teamName: "TeamA"
-      },
-      {
-        id: 4,
-        fullName: "Sam Reynolds",
-        designation: "React Developer",
-        gender: "male",
-        teamName: "TeamB"
-      },
-      {
-        id: 5,
-        fullName: "David Henry",
-        designation: "DotNet Developer",
-        gender: "male",
-        teamName: "TeamB"
-      },
-      {
-        id: 6,
-        fullName: "Sarah Blake",
-        designation: "SQL Server DBA",
-        gender: "female",
-        teamName: "TeamB"
-      },
-      {
-        id: 7,
-        fullName: "James Bennet",
-        designation: "Angular Developer",
-        gender: "male",
-        teamName: "TeamC"
-      },
-      {
-        id: 8,
-        fullName: "Jessica Faye",
-        designation: "API Developer",
-        gender: "female",
-        teamName: "TeamC"
-      },
-      {
-        id: 9,
-        fullName: "Lita Stone",
-        designation: "C++ Developer",
-        gender: "female",
-        teamName: "TeamC"
-      },
-      {
-        id: 10,
-        fullName: "Daniel Young",
-        designation: "Python Developer",
-        gender: "male",
-        teamName: "TeamD"
-      },
-      {
-        id: 11,
-        fullName: "Adrian Jacobs",
-        designation: "Vue Developer",
-        gender: "male",
-        teamName: "TeamD"
-      },
-      {
-        id: 12,
-        fullName: "Devin Monroe",
-        designation: "Graphic Designer",
-        gender: "male",
-        teamName: "TeamD"
-      }])
-
-      function handleTeamSelectionChange(event){
-        console.log(event.target.value);
-        setTeam(event.target.value);
-      }
-
-      function handleEmployeeCardClick(event){
-        const transformedEmployees = employees.map((employee) => employee.id === parseInt(event.currentTarget.id)
-        ?(employee.teamName === selectedTeam)?{...employee, teamName: ''}:{...employee, teamName: selectedTeam}
-        :employee);
-
-        setEmployees(transformedEmployees); 
-
-      }
-
+// Destructuring props, instead of passing props as a parameter in the Employees component
+// We are destructuring the object into its individual property values
+// Through destructing we don't have to write things such as props.employees
+// We are unpacking values properties from objects into distinct variables 
+const Employees = ({employees, selectedTeam, handleEmployeeCardClick, handleTeamSelectionChange}) => {
 
     return (
         <main className="container">

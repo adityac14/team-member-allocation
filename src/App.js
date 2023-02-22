4470884c3fe6205b217c669d8189de239bceb40b
Aditya <adityachatto14@gmail.com>
2023-02-22 15:52:12 -0500

    Added Routing for Header, Footer, Employee components

--- src/App.js
+++ src/App.js
@@ -3,113 +3,115 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Employees from "./Employees";
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   // || - OR operator in JS checks if the first condition is true, if so it doesn't evne look at the next condition
-  const [selectedTeam, setTeam] = useState(JSON.parse(localStorage.getItem('selectedTeam')) || "TeamB");
+  const [selectedTeam, setTeam] = useState(
+    JSON.parse(localStorage.getItem("selectedTeam")) || "TeamB"
+  );
 
   // employees = employee data. the state of the employee data is maintained in the employees variable
   // setEmployees is a function reference and is used where ever we need to change the state of the employees array
   // useState hook returns a pair of values the current state and a function that updates it
 
   // The state of our application will now be persisted to our browser's local storage
-  const [employees, setEmployees] = useState(JSON.parse(localStorage.getItem('employeeList')) ||[
-    {
-      id: 1,
-      fullName: "Bob Jones",
-      designation: "JavaScript Developer",
-      gender: "male",
-      teamName: "TeamA",
-    },
-    {
-      id: 2,
-      fullName: "Jill Bailey",
-      designation: "Node Developer",
-      gender: "female",
-      teamName: "TeamA",
-    },
-    {
-      id: 3,
-      fullName: "Gail Shepherd",
-      designation: "Java Developer",
-      gender: "female",
-      teamName: "TeamA",
-    },
-    {
-      id: 4,
-      fullName: "Sam Reynolds",
-      designation: "React Developer",
-      gender: "male",
-      teamName: "TeamB",
-    },
-    {
-      id: 5,
-      fullName: "David Henry",
-      designation: "DotNet Developer",
-      gender: "male",
-      teamName: "TeamB",
-    },
-    {
-      id: 6,
-      fullName: "Sarah Blake",
-      designation: "SQL Server DBA",
-      gender: "female",
-      teamName: "TeamB",
-    },
-    {
-      id: 7,
-      fullName: "James Bennet",
-      designation: "Angular Developer",
-      gender: "male",
-      teamName: "TeamC",
-    },
-    {
-      id: 8,
-      fullName: "Jessica Faye",
-      designation: "API Developer",
-      gender: "female",
-      teamName: "TeamC",
-    },
-    {
-      id: 9,
-      fullName: "Lita Stone",
-      designation: "C++ Developer",
-      gender: "female",
-      teamName: "TeamC",
-    },
-    {
-      id: 10,
-      fullName: "Daniel Young",
-      designation: "Python Developer",
-      gender: "male",
-      teamName: "TeamD",
-    },
-    {
-      id: 11,
-      fullName: "Adrian Jacobs",
-      designation: "Vue Developer",
-      gender: "male",
-      teamName: "TeamD",
-    },
-    {
-      id: 12,
-      fullName: "Devin Monroe",
-      designation: "Graphic Designer",
-      gender: "male",
-      teamName: "TeamD",
-    },
-  ]);
-
+  const [employees, setEmployees] = useState(
+    JSON.parse(localStorage.getItem("employeeList")) || [
+      {
+        id: 1,
+        fullName: "Bob Jones",
+        designation: "JavaScript Developer",
+        gender: "male",
+        teamName: "TeamA",
+      },
+      {
+        id: 2,
+        fullName: "Jill Bailey",
+        designation: "Node Developer",
+        gender: "female",
+        teamName: "TeamA",
+      },
+      {
+        id: 3,
+        fullName: "Gail Shepherd",
+        designation: "Java Developer",
+        gender: "female",
+        teamName: "TeamA",
+      },
+      {
+        id: 4,
+        fullName: "Sam Reynolds",
+        designation: "React Developer",
+        gender: "male",
+        teamName: "TeamB",
+      },
+      {
+        id: 5,
+        fullName: "David Henry",
+        designation: "DotNet Developer",
+        gender: "male",
+        teamName: "TeamB",
+      },
+      {
+        id: 6,
+        fullName: "Sarah Blake",
+        designation: "SQL Server DBA",
+        gender: "female",
+        teamName: "TeamB",
+      },
+      {
+        id: 7,
+        fullName: "James Bennet",
+        designation: "Angular Developer",
+        gender: "male",
+        teamName: "TeamC",
+      },
+      {
+        id: 8,
+        fullName: "Jessica Faye",
+        designation: "API Developer",
+        gender: "female",
+        teamName: "TeamC",
+      },
+      {
+        id: 9,
+        fullName: "Lita Stone",
+        designation: "C++ Developer",
+        gender: "female",
+        teamName: "TeamC",
+      },
+      {
+        id: 10,
+        fullName: "Daniel Young",
+        designation: "Python Developer",
+        gender: "male",
+        teamName: "TeamD",
+      },
+      {
+        id: 11,
+        fullName: "Adrian Jacobs",
+        designation: "Vue Developer",
+        gender: "male",
+        teamName: "TeamD",
+      },
+      {
+        id: 12,
+        fullName: "Devin Monroe",
+        designation: "Graphic Designer",
+        gender: "male",
+        teamName: "TeamD",
+      },
+    ]
+  );
 
   //
   useEffect(() => {
-    localStorage.setItem('employeeList', JSON.stringify(employees));
-
+    localStorage.setItem("employeeList", JSON.stringify(employees));
   }, [employees]);
 
   useEffect(() => {
-    localStorage.setItem('selectedTeam', JSON.stringify(selectedTeam));
-
+    localStorage.setItem("selectedTeam", JSON.stringify(selectedTeam));
   }, [selectedTeam]);
 
   function handleTeamSelectionChange(event) {
@@ -131,19 +133,38 @@ function App() {
 
   return (
     <div>
-      <Header
-        selectedTeam={selectedTeam}
-        teamMemberCount={employees
-          .filter((employee) => employee.teamName === selectedTeam)
-          .length}
-      />
-      <Employees
-        employees={employees}
-        selectedTeam={selectedTeam}
-        handleEmployeeCardClick={handleEmployeeCardClick}
-        handleTeamSelectionChange={handleTeamSelectionChange}
-      />
-      <Footer />
+      <Router>
+        <Header
+          selectedTeam={selectedTeam}
+          teamMemberCount={
+            employees.filter((employee) => employee.teamName === selectedTeam)
+              .length
+          }
+        />
+        <Routes>
+          {/* Each component we want to navigate to will have its own route element. We want only the Employee component along with the Header & Footer components 
+          when the user first loads the application, when the URL doesn't contain any path information. Then we add an element property within the relevant route element
+          and set this to our employees element*/}
+          <Route
+            path="/"
+            element={
+              <Employees
+                employees={employees}
+                selectedTeam={selectedTeam}
+                handleEmployeeCardClick={handleEmployeeCardClick}
+                handleTeamSelectionChange={handleTeamSelectionChange}
+              />
+            }
+          ></Route>
+          {/* If path contains /GroupedTeamMembers than the GroupedTeamMembers component along with the Heander and Footer components will be rendered to the UI*/}
+          <Route
+            path="/GroupedTeamMembers"
+            element={<GroupedTeamMembers />}
+          ></Route>
+        </Routes>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
